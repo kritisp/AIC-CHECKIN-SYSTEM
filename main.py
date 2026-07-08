ee758ac47f15bfc4e7a3261140b246e3df28d47b
KRITI SREYASH PARIDA <kspchimun@gmail.com>
2026-07-08 15:20:28 +0530

    update cors origins

--- main.py
+++ main.py
@@ -16,6 +16,9 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
         "https://aic-checkin-system.vercel.app",
         "https://aic-scanner.vercel.app"
     ],
